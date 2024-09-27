1b058a2adbc3af9f2168d374cb9cdcabe566c49a
Mallow` <mallownly@gmail.com>
2024-09-27 23:13:17 +0700

    Update main.js

--- Cek kodam/assets/main.js
+++ Cek kodam/assets/main.js
@@ -1,40 +1,47 @@
-    function cekKhodam(event) {
-      event.preventDefault();
-
-      let name = $('#nama').val();
-      if (name === "") {
-        alert("NAMANYA DIISI WOI!!");
-      } else {
-        const status = ["isi", "kosong"];
-        const randomStatus = Math.floor(Math.random() * status.length);
-
-        if (randomStatus === 0) {
-          const khodam = [
-            'Amba',
-            'Gojo Satoru',
-            'Kuda Negro',
-            'Laba Laba Sunda',
-            'Daffa',
-            'Mewing Wotabul',
-            'Sukiliar',
-            'Shiroko Bugil',
-            'Botol Kecap',
-            'Atmosfer Bumi',
-            'Jawa Suki',
-            'Rahmat Fushiguro',
-            'Mas Herman Penjaga Toleransi Beragama',
-            'Jamin Merdu'
-          ];
-          const randomKhodam = Math.floor(Math.random() * khodam.length);
-          $('#result').html(`${name.toUpperCase()} • ${khodam[randomKhodam].toUpperCase()}`);
-        } else {
-          $('#result').html(`${name.toUpperCase()} • KOSONG 😂`);
-        }
-
-        $('#nama').val('');
+    function suki() {
+      const nama = document.getElementById('namaLengkap').value;
+      if (!nama) {
+        alert("NAMA LU BELUM DIISI KOCAK 😂");
+        return false;
       }
+
+      const kodam = khodam(nama);
+      document.getElementById('nama').innerHTML = (`
+        <p>NAMA : <br>${nama.toUpperCase()}</p>
+        <p>KHODAM : <br>${kodam.toUpperCase()}</p>
+      `);
+
+      document.getElementById("popup-1").classList.add("active");
+
+      document.getElementById("namaLengkap").value = "";
+      return false;
     }
 
-    $(document).ready(function() {
-      $('#btnNama').click(cekKhodam);
-    });
+    function closePopup() {
+      document.getElementById("popup-1").classList.remove("active");
+    }
+
+    function khodam(nama) {
+      const khodam = [
+        'Amba',
+        'Gojo Satoru',
+        'Kuda Negro',
+        'Laba Laba Sunda',
+        'Daffa Suki',
+        'Mewing Wotabul',
+        'Sukiliar',
+        'Shiroko Bugil',
+        'Botol Kecap',
+        'Atmosfer Bumi',
+        'Jawa Suki',
+        'Rahmat Fushiguro',
+        'Mas Herman Penjaga Toleransi Beragama',
+        'Jamin Merdu',
+        'Belatung Miskin',
+        'Hendrik Darksistem',
+        'KOSONG 😂'
+      ];
+
+      const index = nama.length % khodam.length;
+      return khodam[index];
+    }
